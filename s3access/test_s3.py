e861259c20b56d3374b8e5ea59f91475303f1e3b
rohanbansal <rohanrb123@gmail.com>
2020-11-28 11:18:09 -0500

    fixed s3 loading script

--- s3access/test_s3.py
+++ s3access/test_s3.py
@@ -12,51 +12,54 @@ with open('config.json', 'r') as f:
 s3=boto3.client('s3')
 
 def process_h5_file(filename):
-    # h5tocopy = open_h5_file_read(filename)
-    # song_num = get_num_songs(h5tocopy)
-    # song_list=[]
-    # for songidx in range(song_num):
-    #     song_info = []
-    #     # METADATA
-    #     song_info.append(float(get_artist_familiarity(h5tocopy,songidx)))
-    #     song_info.append(float(get_artist_hotttnesss(h5tocopy,songidx)))
-    #     song_info.append(str(get_artist_id(h5tocopy,songidx)))
-    #     song_info.append(str(get_artist_location(h5tocopy,songidx)))
-    #     song_info.append(get_artist_mbtags(h5tocopy,songidx).tolist())
-    #     song_info.append(get_artist_mbtags_count(h5tocopy,songidx).tolist())
-    #     song_info.append(str(get_artist_name(h5tocopy,songidx)))
-    #     song_info.append(get_artist_terms(h5tocopy,songidx).tolist())
-    #     song_info.append(get_artist_terms_freq(h5tocopy,songidx).tolist())
-    #     song_info.append(get_artist_terms_weight(h5tocopy,songidx).tolist())
-    #     song_info.append(float(get_danceability(h5tocopy,songidx)))
-    #     song_info.append(float(get_duration(h5tocopy,songidx)))
-    #     song_info.append(float(get_end_of_fade_in(h5tocopy,songidx)))
-    #     song_info.append(float(get_energy(h5tocopy,songidx)))
-    #     song_info.append(float(get_key(h5tocopy,songidx)))
-    #     song_info.append(float(get_key_confidence(h5tocopy,songidx)))
-    #     song_info.append(float(get_loudness(h5tocopy,songidx)))
-    #     song_info.append(float(get_mode(h5tocopy,songidx)))
-    #     song_info.append(float(get_mode_confidence(h5tocopy,songidx)))
-    #     song_info.append(str(get_release(h5tocopy,songidx)))
-    #     song_info.append(get_segments_confidence(h5tocopy,songidx).tolist())        
-    #     song_info.append(get_segments_loudness_max(h5tocopy,songidx).tolist())        
-    #     song_info.append(get_segments_loudness_max_time(h5tocopy,songidx).tolist())    
-    #     song_info.append(get_segments_pitches(h5tocopy,songidx).tolist())    
-    #     song_info.append(get_segments_timbre(h5tocopy,songidx).tolist())    
-    #     song_info.append(get_similar_artists(h5tocopy,songidx).tolist())   
-    #     song_info.append(float(get_artist_hotttnesss(h5tocopy,songidx)))
-    #     song_info.append(str(get_song_id(h5tocopy,songidx)))
-    #     song_info.append(float(get_start_of_fade_out(h5tocopy,songidx)))
-    #     song_info.append(float(get_tempo(h5tocopy,songidx)))
-    #     song_info.append(int(get_time_signature(h5tocopy,songidx)))
-    #     song_info.append(float(get_time_signature_confidence(h5tocopy,songidx)))
-    #     song_info.append(str(get_title(h5tocopy,songidx)))
-    #     song_info.append(str(get_track_id(h5tocopy,songidx)))
-    #     song_info.append(int(get_year(h5tocopy,songidx)))
-    #     song_list.append(song_info)
-    
-    # h5tocopy.close()
-    return filename['metadata']['songs'][:1]['artist_familiarity'][0]
+    h5File = open_h5_file_read(filename)
+    # print(h5File['metadata'].keys())
+    song_num = get_num_songs(h5File)    
+    result = []
+    for songidx in range(song_num):
+        song_info = []
+        # METADATA
+        song_info.append(float(get_artist_familiarity(h5File,songidx)))
+        song_info.append(float(get_artist_hotttnesss(h5File,songidx)))
+        song_info.append(str(get_artist_id(h5File,songidx)))
+        song_info.append(str(get_artist_location(h5File,songidx)))
+        song_info.append(get_artist_mbtags(h5File,songidx).tolist())
+        song_info.append(get_artist_mbtags_count(h5File,songidx).tolist())
+        song_info.append(str(get_artist_name(h5File,songidx)))
+        song_info.append(get_artist_terms(h5File,songidx).tolist())
+        song_info.append(get_artist_terms_freq(h5File,songidx).tolist())
+        song_info.append(get_artist_terms_weight(h5File,songidx).tolist())
+        song_info.append(float(get_danceability(h5File,songidx)))
+        song_info.append(float(get_duration(h5File,songidx)))
+        song_info.append(float(get_end_of_fade_in(h5File,songidx)))
+        song_info.append(float(get_energy(h5File,songidx)))
+        song_info.append(float(get_key(h5File,songidx)))
+        song_info.append(float(get_key_confidence(h5File,songidx)))
+        song_info.append(float(get_loudness(h5File,songidx)))
+        song_info.append(float(get_mode(h5File,songidx)))
+        song_info.append(float(get_mode_confidence(h5File,songidx)))
+        song_info.append(str(get_release(h5File,songidx)))
+        song_info.append(get_segments_confidence(h5File,songidx).tolist())        
+        song_info.append(get_segments_loudness_max(h5File,songidx).tolist())        
+        song_info.append(get_segments_loudness_max_time(h5File,songidx).tolist())    
+        song_info.append(get_segments_pitches(h5File,songidx).tolist())    
+        song_info.append(get_segments_timbre(h5File,songidx).tolist())    
+        song_info.append(get_similar_artists(h5File,songidx).tolist())   
+        song_info.append(float(get_artist_hotttnesss(h5File,songidx)))
+        song_info.append(str(get_song_id(h5File,songidx)))
+        song_info.append(float(get_start_of_fade_out(h5File,songidx)))
+        song_info.append(float(get_tempo(h5File,songidx)))
+        song_info.append(int(get_time_signature(h5File,songidx)))
+        song_info.append(float(get_time_signature_confidence(h5File,songidx)))
+        song_info.append(str(get_title(h5File,songidx)))
+        song_info.append(str(get_track_id(h5File,songidx)))
+        song_info.append(int(get_year(h5File,songidx)))
+
+        result.append(song_info)
+    h5File.close()
+    return result   
+
+    # return filename['metadata']['songs'][:1]['artist_familiarity']
    
 
 
@@ -88,15 +91,14 @@ def transform_s3(key, bucket="songsbuckettest"):
         s3.download_fileobj(bucket, key, tmp)
     
         try:
-            with h5py.File(tmp.name) as h5:
-                return process_h5_file(h5)
+            return process_h5_file(tmp.name)
         except Exception as e:
             print(str(e))
             return []
 
 def main():
     bucket = "songsbuckettest"
-    prefix  = "data/A/R/A/"
+    prefix  = "data/A/R/F/"
     processed=[]
     for prefix in get_prefixes(bucket,prefix):
         processed.append(transform_s3(prefix))
